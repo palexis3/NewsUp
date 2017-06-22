11249c895f3d04b6819691c5a08e8d8661b2b635
palexis3 <alexispatrick77@gmail.com>
2017-06-22 03:33:07 -0700

    Implemented dialog fragment

--- app/src/main/java/com/example/palexis3/newssum/Fragments/FilterDialogFragment.java
+++ app/src/main/java/com/example/palexis3/newssum/Fragments/FilterDialogFragment.java
@@ -14,6 +14,10 @@ import com.example.palexis3.newssum.R;
 
 public class FilterDialogFragment extends DialogFragment {
 
+    private Spinner countrySpinner;
+    private Spinner languageSpinner;
+    private Spinner categorySpinner;
+
     // empty constructor
     public FilterDialogFragment() {};
 
@@ -31,13 +35,50 @@ public class FilterDialogFragment extends DialogFragment {
         return inflater.inflate(R.layout.fragment_filter, container);
     }
 
+    /*
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        String title = getArguments().getString("title");
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(title);
+        builder.setMessage("Filter");
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View view = inflater.inflate(R.layout.fragment_filter, null);
+        builder.setView(view);
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String language = languageSpinner.getSelectedItem().toString();
+                String country = countrySpinner.getSelectedItem().toString();
+                String category = categorySpinner.getSelectedItem().toString();
+                String res = String.format("Language: %s, Country: %s, Category: %s", language, country, category);
+                Toast.makeText(getContext(), res, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        return builder.create();
+    }
+    */
+
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Spinner countrySpinner = (Spinner) view.findViewById(R.id.spinnerCountry);
-        Spinner languageSpinner = (Spinner) view.findViewById(R.id.spinnerLanguage);
-        Spinner categorySpinner = (Spinner) view.findViewById(R.id.spinnerCategory);
+        countrySpinner = (Spinner) view.findViewById(R.id.spinnerCountry);
+        languageSpinner = (Spinner) view.findViewById(R.id.spinnerLanguage);
+        categorySpinner = (Spinner) view.findViewById(R.id.spinnerCategory);
 
         // create adapters for populating spinners with appropriate items
         ArrayAdapter<CharSequence> countryAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.countries_array,
