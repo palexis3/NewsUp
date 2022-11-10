9d26cb75a617f16aeba3e8bba133535a426fd693
Patrick Alexis <alexispatrick77@gmail.com>
2022-11-09 16:48:10 -0800

    Add proper error code handle to showcase web loading error message

--- app/src/main/java/com/example/palexis3/newssum/composable/WebViewScreen.kt
+++ app/src/main/java/com/example/palexis3/newssum/composable/WebViewScreen.kt
@@ -2,18 +2,25 @@ package com.example.palexis3.newssum.composable
 
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
+import android.os.Build
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.palexis3.newssum.R
 
 @Composable
 fun WebViewScreen(
@@ -30,14 +37,27 @@ fun WebViewScreen(
                 )
             }
 
-            ShowWebView(url = url)
+            var errorMessageVisible by remember { mutableStateOf(false) }
+
+            if (errorMessageVisible) {
+                Spacer(Modifier.height(16.dp))
+                Text(
+                    text = stringResource(R.string.webview_loading_error),
+                    style = MaterialTheme.typography.labelMedium,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(CenterHorizontally)
+                )
+            } else {
+                ShowWebView(url = url, errorOccurred = { errorMessageVisible = true })
+            }
         }
     }
 }
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun ShowWebView(url: String) {
+fun ShowWebView(url: String, errorOccurred: () -> Unit) {
     val context = LocalContext.current
     var isLoadingIconVisible by remember { mutableStateOf(false) }
 
@@ -45,7 +65,8 @@ fun ShowWebView(url: String) {
         AndroidView(factory = {
             WebView(context).apply {
                 val loadingWebViewClient = LoadingWebViewClient(
-                    loadingState = { isLoading -> isLoadingIconVisible = isLoading }
+                    loadingState = { isLoading -> isLoadingIconVisible = isLoading },
+                    errorOccurred = errorOccurred
                 )
                 webViewClient = loadingWebViewClient
                 loadUrl(url)
@@ -60,8 +81,21 @@ fun ShowWebView(url: String) {
 }
 
 class LoadingWebViewClient(
-    private val loadingState: (Boolean) -> Unit
+    private val loadingState: (Boolean) -> Unit,
+    private val errorOccurred: () -> Unit
 ) : WebViewClient() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onReceivedError(
+        view: WebView?,
+        request: WebResourceRequest?,
+        error: WebResourceError?
+    ) {
+        if (error?.errorCode == ERROR_CONNECT || error?.errorCode == ERROR_FILE_NOT_FOUND || error?.errorCode == ERROR_IO) {
+            errorOccurred()
+        }
+        super.onReceivedError(view, request, error)
+    }
 
     override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
         super.onPageStarted(view, url, favicon)
