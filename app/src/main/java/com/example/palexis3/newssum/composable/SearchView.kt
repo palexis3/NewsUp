f22228cdf1227ab2c0a26d827d55346ecdf71895
Patrick Alexis <alexispatrick77@gmail.com>
2022-11-14 19:47:52 -0800

    Added sortby functionality with news query

--- app/src/main/java/com/example/palexis3/newssum/composable/SearchView.kt
+++ app/src/main/java/com/example/palexis3/newssum/composable/SearchView.kt
@@ -1,6 +1,7 @@
 package com.example.palexis3.newssum.composable
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,9 +14,13 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -26,9 +31,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -41,6 +48,8 @@ import com.example.palexis3.newssum.models.news_api.NewsApiArticle
 import com.example.palexis3.newssum.state.ArticlesState
 import com.example.palexis3.newssum.viewmodels.ArticleViewModel
 
+private val SORT_BY = listOf("relevancy", "popularity", "publishedAt")
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchView(
@@ -49,10 +58,11 @@ fun SearchView(
     closeScreen: () -> Unit
 ) {
     var searchText by rememberSaveable { mutableStateOf<String?>(null) }
+    var sortBy by rememberSaveable { mutableStateOf<String?>(null) }
 
-    LaunchedEffect(searchText) {
+    LaunchedEffect(key1 = searchText, key2 = sortBy) {
         if (searchText.isNullOrEmpty().not()) {
-            articleViewModel.search(keyword = searchText)
+            articleViewModel.search(keyword = searchText, sortBy = sortBy)
         } else {
             articleViewModel.resetSearch()
         }
@@ -79,7 +89,6 @@ fun SearchView(
             Spacer(modifier = Modifier.width(4.dp))
 
             OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
                 value = searchText ?: "",
                 singleLine = true,
                 onValueChange = { value ->
@@ -107,6 +116,11 @@ fun SearchView(
                 },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
             )
+
+            SortDropDown(
+                currentSelectedItem = sortBy,
+                selected = { item -> sortBy = item }
+            )
         }
 
         when (articleState) {
@@ -121,14 +135,14 @@ fun SearchView(
                         }
                     )
                 } else {
-                    SearchTextError()
+                    SearchTextError(modifier = Modifier.align(CenterHorizontally))
                 }
             }
             is Uninitialized -> {
                 // if the article search state has not been initialized and there's text being searched for
                 // show an error message
                 if (searchText.isNullOrEmpty().not()) {
-                    SearchTextError()
+                    SearchTextError(modifier = Modifier.align(CenterHorizontally))
                 }
             }
             else -> {}
@@ -137,13 +151,51 @@ fun SearchView(
 }
 
 @Composable
-fun SearchTextError() {
-    Column {
-        Text(
-            modifier = Modifier.align(CenterHorizontally),
-            text = stringResource(id = R.string.search_results_not_found)
-        )
+fun SortDropDown(
+    currentSelectedItem: String?,
+    selected: (String) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box {
+        IconButton(onClick = { expanded = true }) {
+            Icon(
+                imageVector = Icons.Filled.Menu,
+                contentDescription = "Sort By"
+            )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            SORT_BY.forEach { item ->
+                DropdownMenuItem(
+                    text = {
+                        Row(verticalAlignment = CenterVertically) {
+                            Text(item)
+                            if (currentSelectedItem == item) {
+                                Spacer(Modifier.width(2.dp))
+                                Icon(
+                                    imageVector = Icons.Default.Check,
+                                    contentDescription = "Checked item"
+                                )
+                            }
+                        }
+                    },
+                    onClick = { selected(item) }
+                )
+            }
+        }
     }
+}
+
+@Composable
+fun SearchTextError(modifier: Modifier = Modifier) {
+    Text(
+        modifier = modifier,
+        text = stringResource(id = R.string.search_results_not_found)
+    )
 }
 
 @Composable
